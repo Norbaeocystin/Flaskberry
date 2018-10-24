6548ac4d542c6750a2b6166483a0e1aee6c8aba7
Norbaeocystin <baranrastislav@gmail.com>
2018-10-24 20:00:25 +0200

    Update LED.py

--- Controlberry/Controlberry/LED.py
+++ Controlberry/Controlberry/LED.py
@@ -6,19 +6,19 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-running = None
+running = {}
 
-def get_light(pin, brightness):
+def get_light(name, pin, brightness):
   '''
   pin number of GPIO pin, brightness = from 0 to 100
   '''
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(pin, GPIO.OUT)
   p = GPIO.PWN(pin,124)
-  running = p
+  running.update({name:p})
   t = Thread(target = p.start, args = brightness)
   t.start()
   
-def stop():
-  if running:
-    running.stop()
+def stop(name):
+  if running.get(name):
+    running.get(name).stop()
