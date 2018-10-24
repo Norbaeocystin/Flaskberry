e33bdb9d6ece861367a419cdb781187118da1b01
Norbaeocystin <baranrastislav@gmail.com>
2018-10-24 21:05:07 +0200

    Update temperature.py

--- Controlberry/Controlberry/temperature.py
+++ Controlberry/Controlberry/temperature.py
@@ -52,7 +52,8 @@ def get_data():
 	'''
 	for each termosensor returns id and temperature
 	'''
-	return {{item.rsplit('/',1)[-1]:get_temperature(item + '/w1_slave')} for item in device_folders}
+	result = {}
+	return [result.update({item.rsplit('/',1)[-1]:get_temperature(item + '/w1_slave')}) for item in device_folders]
         
 
 def insert_into_database():
