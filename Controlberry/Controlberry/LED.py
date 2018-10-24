48e13c5c1f5ab432d2af791c255d799dddc1d6e5
Norbaeocystin <baranrastislav@gmail.com>
2018-10-24 20:16:31 +0200

    Update LED.py

--- Controlberry/Controlberry/LED.py
+++ Controlberry/Controlberry/LED.py
@@ -1,17 +1,41 @@
 '''
 For control of LEDs, LED stripes
 '''
-from threading import Thread
+import json
+from pymongo import MongoClient
 import RPi.GPIO as GPIO
+import time
 
+#loads config file
+json_data= open('config.json').read()
+DATABASE = json.loads(json_data)
+URI = DATABASE.get('URI')
+DB = DATABASE.get('Database')
+
+#setup mongodb
+CONNECTION = MongoClient(URI, connect = False)
+db = CONNECTION.get_database(DB)
+Settings = db.Settings.find_one({"_id":0},{'_id':0})
+
+#GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
 
 running = {}
 
-def get_light(name, pin, brightness):
+
+def get_pin(name):
+    '''
+    LedName_0_ returns pin
+    '''
+    pin = name.replace('Name', 'Pin')
+    return pin
+
+
+def get_light(name, brightness):
   '''
   pin number of GPIO pin, brightness = from 0 to 100
   '''
+  pin = get_pin(name)
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(pin, GPIO.OUT)
   p = GPIO.PWN(pin,124)
