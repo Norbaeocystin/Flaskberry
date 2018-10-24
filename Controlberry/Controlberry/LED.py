6518d87f7a94dc8d949d10f58a1d0135eed72bc7
Norbaeocystin <baranrastislav@gmail.com>
2018-10-24 23:44:50 +0200

    Update LED.py

--- Controlberry/Controlberry/LED.py
+++ Controlberry/Controlberry/LED.py
@@ -59,7 +59,4 @@ def get_light(name, brightness = 100):
 def get_light_stop(name):
     if running.get(name):
         running.get(name).stop()
-        pin = get_pin(name)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.cleanup()
+        del running[name]
