b706756364ec956ae210cde0be775354cf66d3e3
Norbaeocystin <baranrastislav@gmail.com>
2018-10-25 20:23:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="controlberry_pkg",
+    name="flaskberry_pkg",
     version="0.0.1",
     author="Rastislav_Baran",
     author_email="baranrastislav@gmail.com",
-    description="Package which needs to be installed on Raspberry Pi 3 to control it via Flaskberry",
+    description="Flaskberry web app to control Raspberry Pi with Controlberry",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
