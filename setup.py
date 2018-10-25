81cb6eed8a7e317afd2a15ae8c22a249321adbb6
Norbaeocystin <baranrastislav@gmail.com>
2018-10-25 21:05:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flaskberry",
+    name="Flaskberry",
     version="0.0.1",
     author="Rastislav_Baran",
     author_email="baranrastislav@gmail.com",
-    description="Flaskberry web app to control Raspberry Pi with Controlberry",
+    description="Flask web app to control Raspberry Pi with Controlberry",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
