0333363b34ed3ea0577d5940f9c04460ef2a0cf9
Norbaeocystin <baranrastislav@gmail.com>
2018-10-25 21:35:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="",
     packages=setuptools.find_packages(),
+    package_data={'Flaskberry': ['*.html', 'Templates/*.html']}
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
