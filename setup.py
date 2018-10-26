fe2e630e6e6f0f26480c83e8cb568e8accfb98ab
Norbaeocystin <baranrastislav@gmail.com>
2018-10-26 17:07:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Flaskberry",
+    install_requires=["pymongo","Flask"],
     version="0.0.1",
     author="Rastislav_Baran",
     author_email="baranrastislav@gmail.com",
