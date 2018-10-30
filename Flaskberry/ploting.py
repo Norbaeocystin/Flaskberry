ef8bd9a7c58d841dfcb2559873aaf4f6269a23f1
Norbaeocystin <baranrastislav@gmail.com>
2018-10-30 08:04:24 +0100

    Add files via upload

--- Flaskberry/ploting.py
+++ Flaskberry/ploting.py
@@ -52,31 +52,28 @@ def transform_adafruit_dict(json, keys = keys, settings = settings):
     return result
 
 temperatures = list(Adafruit.find({'Timestamp':{'$gt':delta}},{'_id':0}))
-first = (temperatures[-1].get('Timestamp') - now).seconds//3600
 tmp  = [transform_adafruit_dict(item) for item in temperatures]
 df = pandas.DataFrame(tmp)
 
 def get_image_as_string(df = df, humidity = 'Room Humidity', temperature = 'Room Temperature', 
                         targets_t = (30,20), targets_h = (60,40), day_start = 10, day_end = 20, 
-                        h = first, humidity_interval = (0, 100), temperature_interval = (0,40)):
+                       humidity_interval = (0, 100), temperature_interval = (0,40)):
     #dividing night and day
-    print(targets_t)
-    print(targets_h)
     target_humidity_day,target_humidity_night  = targets_h
     target_temperature_day, target_temperature_night = targets_t
-    order_hours = list(range(h + 1,-1, -1))
     d = datetime.datetime.now()
-    hours = [(d - datetime.timedelta(hours = item)).hour for item in order_hours]
-    time = df['Timestamp'] + datetime.timedelta(hours = utc_zone)
+    time_ = df['Timestamp'] + datetime.timedelta(hours = utc_zone)
+    hours = list(time_.dt.hour.unique()[1:])#ordered list of hours [15,16,17,18,20,21,22,23,0,1,2 ...]
     temperature = df[temperature]
     humidity = df[humidity]
     hours_ = mdates.HourLocator(interval = 1)
+    minutes_ = df['Timestamp'][0].minute/60
     h_fmt = mdates.DateFormatter('%H:%M')
     fig, ax = plt.subplots(2, figsize = (5,5))
     plt.subplots_adjust(hspace = 0.35)
     #fig.autofmt_xdate()
     # Call plot() method on the appropriate object
-    ax[0].plot(time, humidity)
+    ax[0].plot(time_, humidity)
     ax[0].set_title('Humidity')
     ax[0].set_ylim(humidity_interval)
     #ax[0].set_yticks([30,40,50,60,70,80])
@@ -84,7 +81,7 @@ def get_image_as_string(df = df, humidity = 'Room Humidity', temperature = 'Room
     ax[0].xaxis.set_major_formatter(h_fmt)
     ax[0].xaxis.set_major_locator(plt.MaxNLocator(10))'''
     ax[1].set_title('Temperature')
-    ax[1].plot(time, temperature)
+    ax[1].plot(time_, temperature)
     ax[1].xaxis.set_major_locator(hours_)
     ax[1].set_ylim(temperature_interval)
     ax[1].xaxis.set_major_formatter(h_fmt)
@@ -92,7 +89,7 @@ def get_image_as_string(df = df, humidity = 'Room Humidity', temperature = 'Room
     ax[0].xaxis.set_major_locator(hours_)
     ax[0].xaxis.set_major_formatter(h_fmt)
     ax[0].xaxis.set_major_locator(plt.MaxNLocator(10))
-    def add_line(ax, x1,x2,y1,y2, color ,label):
+    def add_line(ax, x1,x2,y1,y2, color ):#,label = 'Target'):
         '''
         adding line to subplot ax
         '''
@@ -102,14 +99,15 @@ def get_image_as_string(df = df, humidity = 'Room Humidity', temperature = 'Room
     for item in  [[ax[0],target_humidity_day, target_humidity_night],
                   [ax[1], target_temperature_day, target_temperature_night]]:
         min_, max_ = item[0].get_xlim()
+        """
         line_1 = add_line(item[0],min_,max_,item[1],item[1],'red',label='Day Target')
         line_2 = add_line(item[0],min_,max_,item[2],item[2],'green',label='Night Target')
         item[0].legend(handles =[line_1, line_2],labels = ['Day Target', 'Night Target'], loc='lower right', frameon=False)
-        '''
+        """
         #case where day start is in hours limit
         if day_start in hours and day_end not in hours:
             logger.debug('case 1')
-            start = (((max_ - min_)/24)*hours.index(day_start) +2 )+ min_
+            start = (((max_ - min_)/len(hours))*(hours.index(day_start)  + minutes_) )+ min_
             #add lines
             add_line(item[0], start, max_,item[1], item[1], 'red')
             add_line(item[0], start, start,item[1], item[2], 'red')
@@ -117,20 +115,21 @@ def get_image_as_string(df = df, humidity = 'Room Humidity', temperature = 'Room
         #next case  
         if day_start not in hours and day_end in hours:
             logger.debug('case 2')
-            start = (((max_ - min_)/24)*hours.index(day_end)  + 2)+ min_
+            start = (((max_ - min_)/len(hours))*(hours.index(day_end) + minutes_))+ min_
+            print(start)
             #add lines
-            add_line(item[0], start, max_, item[1], item[1], 'red')
+            add_line(item[0], min_, start, item[1], item[1], 'red')
             add_line(item[0], start, start,item[1], item[2], 'red')
-            add_line(item[0], min_, start,item[2], item[2], 'red')
+            add_line(item[0], start, max_,item[2], item[2], 'red')
         #next two cases
         if day_start in hours and day_end in hours:
-            day_start_index = hours.index(day_start)
-            day_end_index = hours.index(day_end)
+            day_start_index = hours.index(day_start) + minutes_
+            day_end_index = hours.index(day_end) + minutes_
             #first one
             if day_end_index > day_start_index:
                 logger.debug('case 3')
-                start = (((max_ - min_)/24)*(day_start_index+1))+ min_
-                end = (((max_ - min_)/24)*(day_end_index +2))+ min_
+                start = (((max_ - min_)/len(hours))*(day_start_index))+ min_
+                end = (((max_ - min_)/len(hours))*(day_end_index))+ min_
                 add_line(item[0], min_, start,item[2], item[2], 'red')
                 add_line(item[0], start, start,item[1], item[2], 'red')
                 add_line(item[0], start, end, item[1], item[1], 'red')
@@ -139,14 +138,13 @@ def get_image_as_string(df = df, humidity = 'Room Humidity', temperature = 'Room
             #second one
             if day_end_index < day_start_index:
                 logger.debug('case 4')
-                start = (((max_ - min_)/24)*(day_start_index+1))+ min_
-                end = (((max_ - min_)/24)*(day_end_index+2))+ min_
+                start = (((max_ - min_)/len(hours))*(day_start_index))+ min_
+                end = (((max_ - min_)/len(hours))*(day_end_index))+ min_
                 add_line(item[0], start, max_, item[1], item[1], 'red')
                 add_line(item[0], start, start,item[1], item[2], 'red')
                 add_line(item[0], end, start, item[2], item[2], 'red')
                 add_line(item[0], end, end,item[1], item[2], 'red')
                 add_line(item[0], min_, end,item[1], item[1], 'red')
-        '''
     buf = io.BytesIO()
     fig.savefig(buf, format='png')
     buf.seek(0)
@@ -168,7 +166,8 @@ def get_key_document(key):
 def get_keys():
     vis_data = Settings.find_one().get('Visualization')
     if vis_data:
-        keys = list({item.replace('__Temperature_day','') for item in vis_data.keys()})
+        keys = list({item.replace('_Temperature_day','') for item in vis_data.keys() if '_Temperature_day' in item})
+        logger.debug(keys)
         return keys
 
 def get_image_as_string_from_key(key):
